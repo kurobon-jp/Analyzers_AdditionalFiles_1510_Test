aaed55795a3a1e42200f3ae0973b2992b6c993cd
kurobon <kurobon>
2019-07-10 22:16:45 +0900

    Initial commit

---
+++ Analyzers_AdditionalFiles_1510_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="Callie LeFave">
+// Copyright (c) Callie LeFave 2019
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// </copyright>
+
+namespace Analyzers_AdditionalFiles_1510_Test
+{
+    /// <summary>
+    /// Program.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Analyzers_AdditionalFiles_1510_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Callie LeFave">
+// Copyright (c) Callie LeFave 2019
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("Analyzers_AdditionalFiles_1510_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Analyzers_AdditionalFiles_1510_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("1e183812-99c0-46c4-9fcd-cfa3c868eaf0")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
